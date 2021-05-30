bec856c8f884fd83daa29f26ea4ed4500d5aeceb
niemet0502 <vincentmarius8@gmail.com>
2021-05-30 23:28:22 +0200

    main :: created SidebarItem component

---
+++ src/components/SidebarItem.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from "styled-components"
+
+function SidebarItem({Icon,text, active}) {
+  return (
+    <SidebarItemContainer>
+      <a href="" className={"link " + (active == "true" ? "active" : "unactive")}>
+        <Icon/>
+        {text}
+      </a>
+    </SidebarItemContainer>
+  )
+}
+const SidebarItemContainer = styled.a`
+ 
+  a {
+    display: flex;
+    align-items: center;
+    margin-left: -2px;
+    text-decoration: none;
+    font-size: 14px;
+    color: #b3b3b3;
+    padding: 3px 3px 3px 10px;
+    border-radius: 5px;
+    margin-bottom: 10px;
+  }
+
+  a .MuiSvgIcon-root{
+    width: 32px;
+    height: 32px;
+    margin-right: 10px;
+  }
+
+   .active{
+    color : white !important;
+    background-color: #282828 !important;
+  }
+
+  a:hover{
+    color : white !important;
+  }
+
+`;
+export default SidebarItem
