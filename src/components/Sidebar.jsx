d534924077516d5a3b50f9f3299755017c27ee83
niemet0502 <vincentmarius8@gmail.com>
2021-05-31 17:11:42 +0200

    main :: added playlists resources

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
 import styled from "styled-components"
 import logo from "../assets/spotifyLogo.png"
+import SidebarItem from "./SidebarItem"
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+
 function Sidebar() {
+
+  const playlists = [
+    {
+      id: 1,
+      title: 'Sad'
+    },
+    {
+      id: 2,
+      title: 'Tired'
+    },
+    {
+      id: 3,
+      title: 'Nija'
+    },{
+      id: 4,
+      title: 'Rap FR'
+    },{
+      id: 5,
+      title: 'Drill'
+    },{
+      id: 6,
+      title: 'Reap US '
+    },{
+      id: 7,
+      title: 'Latino'
+    }
+  ]
+
   return (
     <SidebarComponent>
       <div className="SidebarContainer">
-       <img src={logo} alt="spotify-logo" />
+        <img src={logo} alt="spotify-logo" />
+        <div className="Sidebar__menu">
+          <SidebarItem
+            Icon={HomeIcon}
+            text="Home"
+            active="true"/>
+
+          <SidebarItem
+            Icon={SearchIcon}
+            text="Search"
+          />
+        </div>
+
+        <div className="Sidebar__playlist">
+        {playlists.map(playlist => <SidebarItem text={playlist.title}   />)}
+        </div>
       </div>
     </SidebarComponent>
   )
@@ -15,14 +62,30 @@ const SidebarComponent = styled.div`
   .SidebarContainer{
     background-color: black;
     width: 220px;
-    height: 760px;
+    height: 85%;
     position: sticky;
-    padding: 25px 10px 0px 15px;
+    padding: 25px 10px 0px 20px;
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    z-index: 1;
   }
 
   .SidebarContainer img{
     width: 140px;
-    margin-left: 10px;
+    margin-left: 8px;
+  }
+
+  .Sidebar__menu{
+    display: flex;
+    flex-direction: column;
+    margin-top: 26px;
+  }
+
+  .Sidebar__playlist{
+    border-top: 0.5px solid #b3b3b3;
+    overflow: auto;
+    flex: 2;
   }
 `;
 
